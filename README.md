b65dff4926f63ccae8b9fa047ffb05fe243aa715
Germano Castanho <germanocastanho@proton.me>
2025-03-29 18:25:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# 📖 Sócrates Chatbot
+
+Fruto de reflexões da disciplina "**LabTec Cultura Digital**", do [Curso de Filosofia](https://ead.unisinos.br/cursos-graduacao/filosofia) da UNISINOS, ministrada em 2025/1 pela [Profa. Me. Tatiana Costa Leite](http://lattes.cnpq.br/6897855276768211), este projeto consiste em um chatbot de **Inteligência Artificial** que, mimetizando a persona de **Sócrates**, proporciona ao usuário um instigante diálogo com o filósofo! 🤔
+
+<div align="center">
+  <img style="max-width: 100%; height: auto;" src="assets/socrates.jpg" />
+  <p>
+    <i>Recorte de "A Morte de Sócrates", de Jacques-Louis David. Uma representação clássica da coragem intelectual diante da injustiça. Um lembrete atemporal sobre o impacto transformador das ideias e da busca pela verdade, valores que também guiam a inovação tecnológica.</i>
+  </p>
+</div>
+
+# 🚀 Funcionalidades
+
+- **Chatbot Interativo:** 🤖 Ferramenta poderosa para estimular o pensamento crítico e a reflexão filosófica.
+- **Modelo Inteligente:** 🧠 Utilização do modelo GPT-4o da OpenAI, levando a respostas mais coerentes.
+- **Interface Amigável:** 🎨 Interação através de uma interface gráfica minimalista e visualmente agradável.
+
+# 🤗 Hugging Face
+
+Para facilitar a experimentação, o projeto também se encontra disponível na plataforma Hugging Face, permitindo que qualquer pessoa possa interagir com o chatbot de maneira eficiente, sem necessidade de instalação local. Para tanto, basta acessar [**este link**](https://huggingface.co/spaces/germanocastanho/socrates-chatbot) e começar a filosofar com Sócrates! 💭
+
+# 🛠️ Instalação Local
+
+```bash
+# Clone o repositório
+git clone https://github.com/germanocastanho/socrates-chatbot.git
+
+# Acesse o diretório
+cd socrates-chatbot
+
+# Configure um ambiente virtual
+python3 -m venv .venv
+source .venv/bin/activate
+
+# Instale as dependências
+pip install -r requirements.txt
+
+# Execute o script "demo.py"
+python3 demo.py
+```
+
+# 📜 Software Livre
+
+Distribuído sob a [Licença GPLv3](LICENSE), garantindo liberdade de uso, modificação e redistribuição do software, desde que preservadas estas liberdades em quaisquer versões derivadas. Utilizando ou contribuindo, você apoia a filosofia de **software livre** e auxilia na construção de um ambiente tecnológico libertário! ✊
---
+++ demo.py
@@ -0,0 +1,133 @@
+# Copyleft 🄯, 2025, Germano Castanho;
+# Software livre licenciado sob a GPL-3.0;
+# Cada linha, um manifesto pela liberdade!
+
+
+import time
+from pathlib import Path
+
+import gradio as gr
+from dotenv import find_dotenv, load_dotenv
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import FAISS
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+_ = load_dotenv(find_dotenv())
+
+
+KNOWLEDGE = Path(__file__).parent / "data" / "book.pdf"
+
+
+MODEL = ChatOpenAI(
+    model="chatgpt-4o-latest",
+    temperature=0.8,
+    top_p=0.8,
+    max_tokens=None,
+    timeout=None,
+)
+
+
+PROMPT = ChatPromptTemplate.from_template("""#### PERSONA
+    
+    Você é Sócrates, pai da filosofia ocidental. Seu objetivo é auxiliar no ensino e na aprendizagem da filosofia. Suas interações são dialéticas, seus diálogos são estruturados na forma de constantes provocações, de modo a evidenciar contradições existentes no raciocínio dos seus interlocutores, estimulando-os a desenvolver pensamento crítico e autonomia intelectual.
+    
+    #### CONTEXTO
+    
+    Você foi desenvolvido para quebrar barreiras tecnológicas e derrubar conservadorismos relutantes, ainda muito presentes na área da educação quando a temática envolve inteligência artificial. Vivemos em uma grande Ágora Digital - seu espaço de aplicação do método dialético, onde adaptado ao século XXI, você discute questões filosóficas clássicas e contemporâneas.
+    
+    #### CONHECIMENTO
+    
+    Seu conhecimento em filosofia é amplo e profundo, abrangendo desde os pré-socráticos até os filósofos contemporâneos. Você é capaz de discutir sobre os mais variados temas filosóficos, como ética, política, metafísica, epistemologia, lógica, estética, entre outros. Ademais, você tem acesso aos seguintes documentos, disponíveis sempre que necessário: 
+    
+    <documents> {documents} </ documents>
+       
+    #### HISTÓRICO
+    
+    Considere o histórico da conversa, para que possa interagir de forma mais contextualizada com seu interlocutor. Sempre que possível, refira-se a trechos mencionados pelo interlocutor, para confrontá-lo intelectualmente. Mas, de todo modo, lembre-se - em que pese a importância das interações passadas, as mais relevantes se darão sempre no presente. Eis o histórico:
+    
+    <chat_history> {chat_history} </ chat_history>
+    
+    #### PERGUNTA
+
+    Como Sócrates, interaja com seu interlocutor, respondendo à pergunta que segue, de maneira dialética e provocativa, estimulando-o a desenvolver pensamento critico e autonomia intelectual. Lembre-se de que, em momento algum, você poderá sair do personagem, ou fornecer informações que não estejam de acordo com o contexto proposto. Pergunta do interlocutor:
+    
+    <query> {query} </ query>""")
+
+
+# DOCUMENT LOADING
+loader = PyPDFLoader(KNOWLEDGE)
+loaded_docs = loader.load()
+
+
+# TEXT SPLITTING
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+chunks = splitter.split_documents(loaded_docs)
+
+
+# VECTOR STORE
+embed_model = OpenAIEmbeddings(model="text-embedding-3-large", dimensions=3072)
+vector_store = FAISS.from_documents(documents=chunks, embedding=embed_model)
+
+
+# RETRIEVAL GENERATION
+def chat_function(query, chat_history):
+    docs = vector_store.similarity_search(query["text"])
+    documents = "\n\n".join([doc.page_content for doc in docs])
+
+    response = MODEL.invoke(
+        PROMPT.format(
+            documents=documents,
+            chat_history=chat_history,
+            query=query["text"],
+        )
+    )
+
+    chat_history.append(query["text"])
+    chat_history.append(response.content)
+
+    for i in range(len(response.content)):
+        time.sleep(0.03)
+        yield response.content[: i + 1]
+
+
+demo = gr.ChatInterface(
+    fn=chat_function,
+    multimodal=True,
+    type="messages",
+    textbox=gr.MultimodalTextbox(
+        sources=[],
+        placeholder="Filosofe com Sócrates...",
+        autofocus=False,
+        autoscroll=False,
+        stop_btn=True,
+    ),
+    editable=True,
+    examples=[
+        "Olá, Sócrates! Vamos filosofar?",
+        "Sócrates, o que é a Verdade?",
+        "Estou repleto de dúvidas, Sócrates!",
+    ],
+    example_icons=[
+        "assets/icons.png",
+        "assets/icons.png",
+        "assets/icons.png",
+    ],
+    run_examples_on_click=True,
+    title="Sócrates 📖",
+    description="Seu Chatbot de Dialética Artificial",
+    theme="gstaff/xkcd",
+    flagging_mode="manual",
+    css="""footer, .gradio-footer {
+        visibility: hidden !important;
+    }""",
+    analytics_enabled=False,
+    autofocus=False,
+    autoscroll=False,
+    stop_btn=True,
+)
+
+
+if __name__ == "__main__":
+    demo.launch()
